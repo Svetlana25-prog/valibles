41c8884d6b92c9ace53ac7c83071218673b1a4ac
Svetlana25-prog <skyfall12@mail.ru>
2024-10-07 18:14:40 +0300

    Файл исправлен 07.10.2024

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 count_dz = 12
 count_hours = 1.5
 course_name = 'Python'
-time_one_task = count_dz - count_hours
+time_one_task = count_dz / count_hours
 print(f'Курс: {course_name}, всего задач:{count_dz}, затрачено часов: {count_hours}, среднее время выполнения {time_one_task} часа')
